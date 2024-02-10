f4f6aefce92ee59a26dffb8b1ec58de8bd0cae43
George <georgie.george7@gmail.com>
2024-02-11 00:14:18 +0400

    Refactor expense and group models, add validations and associations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,25 @@
 class Expense < ApplicationRecord
-  belongs_to :user
   belongs_to :group
-  validates :author_id, presence: true
-  validates :name, presence: true
-  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :name, :amount, :date, presence: true
+  validates :name, length: { maximum: 50 }
+  validates :amount, numericality: true
+  validates :group_id, presence: true
+
+  # I have not included the numericality, because it I want to use expenses and income in my app.
+  # The expense is an expense if it is negative.
+
+  # validates :amount, numericality: { greater_than: 0 }
+
+
+
+  # I have not included the date validation because,
+  # if a user wants to plan for the future month's exenses and see what they will have available in budget.
+  # Then they can.
+  # validate :date_cannot_be_in_the_future
+
+  # def date_cannot_be_in_the_future
+  #   if date.present? && date > Date.today
+  #     errors.add(:date, "can't be in the future")
+  #   end
+  # end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,16 @@
 class Group < ApplicationRecord
   has_many :expenses
+  belongs_to :user
+  has_one_attached :icon
+
   validates :name, presence: true
+  validate :valid_icon_url_or_attachment
+
+  private
+
+  def valid_icon_url_or_attachment
+    return if icon.attached?
+
+    errors.add(:base, 'You must provide an icon URL or upload an icon image')
+  end
 end
