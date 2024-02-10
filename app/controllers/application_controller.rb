58662847915a132250d6e0afe56540a9ceb35d54
George <georgie.george7@gmail.com>
2024-02-11 00:12:53 +0400

    Add splash page controller and view, update application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password) }
+  end
+
+  def after_sign_in_path_for(_resource)
+    groups_path
   end
 end
