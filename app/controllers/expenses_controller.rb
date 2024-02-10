7897d2f54dc79a4e682c101fb8961c2cfc1d4b81
George <georgie.george7@gmail.com>
2024-02-11 00:16:09 +0400

    Add authentication and before actions to expenses and groups controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,10 +1,55 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_group, only: %i[new create]
+  before_action :set_expense, only: %i[edit update destroy]
+
   def index
+    @expenses = current_user.expenses.order(date: :desc)
   end
 
   def new
+    @expense = @group.expenses.build
   end
 
   def create
+    @expense = @group.expenses.build(expense_params)
+    @expense.author_id = current_user.id
+    if @expense.save
+      redirect_to group_path(@group), notice: 'Expense created successfully.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @expense = current_user.expenses.find(params[:id])
+    @group = @expense.group
+  end
+
+  def update
+    if @expense.update(expense_params)
+      redirect_to group_path(@expense.group), notice: 'Expense updated successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @expense.destroy
+    redirect_to group_path(@expense.group), notice: 'Expense deleted successfully.'
+  end
+
+  private
+
+  def set_group
+    @group = current_user.groups.find(params[:group_id])
+  end
+
+  def set_expense
+    @expense = current_user.expenses.find(params[:id])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount, :group_id, :date)
   end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,55 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @groups = current_user.groups.order(updated_at: :desc)
   end
 
   def show
+    @group = current_user.groups.find(params[:id])
+    @expenses = @group.expenses.order(created_at: :desc)
+  end
+
+  def edit
+    @group = current_user.groups.find(params[:id])
+  end
+
+  def destroy
+    @group = current_user.groups.find(params[:id])
+    if @group.expenses.any?
+      redirect_to groups_path,
+                  alert: 'Please move or delete the transactions before deleting the category.'
+    else
+      @group.destroy
+      redirect_to groups_path, notice: 'Category deleted successfully.'
+    end
+  end
+
+  def update
+    @group = current_user.groups.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: 'Category updated successfully.'
+    else
+      render :edit
+    end
   end
 
   def new
+    @group = current_user.groups.build
   end
 
   def create
+    @group = current_user.groups.build(group_params)
+    if @group.save
+      redirect_to groups_path, notice: 'Group created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
