9695e7893b30225b01617c411a71217ba9932f86
George <georgie.george7@gmail.com>
2024-02-17 07:41:26 +0400

    Add validation for expense amount range and add testing for it

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -17,5 +17,26 @@ RSpec.describe Expense, type: :model do
       expense = Expense.new(name: 'Example Expense', amount: 50.0, date: Date.today)
       expect(expense).not_to be_valid
     end
+    it 'is not valid if amount is more than 10,000,000' do
+      user = User.create(email: 'test@example.com', password: 'password')
+      user.confirm
+      user.save
+      group = user.groups.create(name: 'Test Group',
+                                  icon: File.open(Rails.root.join('app', 'assets',
+                                                                  'images', 'defaultgroupicon.png')))
+      expense = group.expenses.new(name: 'Example Expense', amount: 10000001, date: Date.today)
+      expect(expense).not_to be_valid
+    end
+    it 'is not valid if amount is less than -10,000,000' do
+      user = User.create(email: 'test@example.com', password: 'password')
+      user.confirm
+      user.save
+      group = user.groups.create(name: 'Test Group',
+                                  icon: File.open(Rails.root.join('app', 'assets',
+                                                                  'images', 'defaultgroupicon.png')))
+      expense = group.expenses.new(name: 'Example Expense', amount: -10000001, date: Date.today)
+      expect(expense).not_to be_valid
+    end
+
   end
 end
